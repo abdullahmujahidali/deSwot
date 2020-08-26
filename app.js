568253cf201b51ffdad54a75d97f260ed13e089c
Abdullah Mujahid  ◘_◘ <48271080+abdullahmujahidali@users.noreply.github.com>
2020-08-26 09:16:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 });
 
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT|| 3000,function(){
+    console.log("Server is running ");
 });
 //API
 //b4a6b0c80eee9f43e7623e46018d0a56-us17
