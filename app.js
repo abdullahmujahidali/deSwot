ab95354faa9b40d3366ad5dcb2a8169bd0f795dc
Abdullah Mujahid  ◘_◘ <48271080+abdullahmujahidali@users.noreply.github.com>
2020-08-26 09:13:59 +0500

    adding files

---
+++ app.js
@@ -0,0 +1,67 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/",function(req,res){
+   var firstName=req.body.fName;
+    var lastName=req.body.lName;
+    var email=req.body.email;
+    console.log(firstName,lastName,email);
+
+    var data={
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME: lastName
+                }
+            }
+        ] 
+     };
+     
+var jsonData=JSON.stringify(data);
+const url= "https://us17.api.mailchimp.com/3.0/lists/f0b4bd4b00";
+const options ={
+    method: "POST",
+    auth: "abdullahmujahidali:b4a6b0c80eee9f43e7623e46018d0a56-us17"
+}
+const request=https.request(url,options,function(response){
+    if (response.statusCode===200){
+        res.sendFile(__dirname+"/success.html");
+    }
+    else{
+        res.sendFile(__dirname+"/failure.html");
+    }
+
+    response.on("data",function(data){
+     console.log(JSON.parse(data));
+ })
+
+});
+request.write(jsonData);
+request.end(); 
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
+//API
+//b4a6b0c80eee9f43e7623e46018d0a56-us17
+
+//list id
+//f0b4bd4b00
